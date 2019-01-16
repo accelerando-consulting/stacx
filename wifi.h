ea8477b9a4042ec07e508e562cbc682621514302
wai <thanmyintwai.my@googlemail.com>
2019-01-16 11:07:10 +1000

    More tweaking of cold startup procesS

--- wifi.h
+++ wifi.h
@@ -49,7 +49,7 @@ void _readConfig()
   NOTICE("mounted file system");
   if (!SPIFFS.exists("config.json")) {
     ALERT("No configuration file found");
-    _writeConfig();
+    _writeConfig(true);
   }
 
   //file exists, reading and loading
