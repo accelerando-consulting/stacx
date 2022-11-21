3cc44bd0efaff1008a57ae55bfbd0d86b4b0b7c5
Christopher Biggs <christopher@accelerando.com.au>
2022-11-22 08:42:14 +1000

    abstract_app.h - factor out common app_use_lte/app_use_wifi logic to abstract app class

--- abstract_app.h
+++ abstract_app.h
@@ -12,6 +12,9 @@ class AbstractAppLeaf : public Leaf
 {
 protected:
   bool app_publish_version = false;
+  bool app_use_lte = true;
+  bool app_use_lte_gps = false;
+  bool app_use_wifi = false;
 
 public:
   AbstractAppLeaf(String name, String target=NO_TAPS)
@@ -44,6 +47,10 @@ public:
     getBoolPref("debug_lines", &debug_lines, "Include line numbers log messages");
     getBoolPref("app_publish_version", &app_publish_version, "Publish version information at first connect");
 
+    getBoolPref("app_use_lte", &app_use_lte, "Enable use of 4G (LTE) modem");
+    getBoolPref("app_use_lte_gps", &app_use_lte_gps, "Enable use of 4G (LTE) modem (for GPS only)");
+    getBoolPref("app_use_wifi", &app_use_wifi, "Enable use of WiFi");
+
 #ifndef ESP8266
     if (wake_reason.startsWith("deepsleep/")) {
       if (saved_sig == RTC_SIG) {
@@ -55,6 +62,87 @@ public:
       }
     }
 #endif
+
+    if (ipLeaf->canRun()) {
+      LEAF_NOTICE("Application will use network transport %s", ipLeaf->describe().c_str());
+    }
+    else {
+      LEAF_NOTICE("No IP active yet, choosing a transport layer to activate");
+
+      // IP is not currently activated, choose one of the available IP leaves to activate
+
+      AbstractIpLTELeaf *lte = NULL;
+      if (app_use_lte || app_use_lte_gps) {
+	lte = (AbstractIpLTELeaf *)find("lte","ip");
+      }
+    
+      if (lte && app_use_lte_gps) {
+	LEAF_NOTICE("LTE will be used for GPS only");
+	lte->permitRun();
+	lte->setup();
+      }
+
+      bool ip_valid = false;
+      if (app_use_lte) {
+	AbstractPubsubLeaf *lte_pubsub =(AbstractPubsubLeaf *)find("ltemqtt", "pubsub");
+	
+	if (lte && lte_pubsub) {
+	  LEAF_WARN("Selecting LTE as preferred communications");
+	  stacxSetComms(lte, lte_pubsub);
+	  ip_valid=true;
+	}
+	else {
+	  LEAF_ALERT("Cannot find LTE ip/pubsub leaf-pair");
+	}
+      }
+      else {
+	LEAF_NOTICE("Use of LTE for comms is not enabled");
+      }
+    
+      AbstractIpLTELeaf *wifi = (AbstractIpLTELeaf *)find("wifi","ip");
+      if (wifi && app_use_wifi && app_use_lte && lte && lte->canRun()) {
+	// The wifi leaf is enabled, but so is LTE.  We presume wifi is to be for SECONDARY comms,
+	// which means it does not advertise its presence to a pubsub leaf.
+	//
+	// Wifi will be used for debugging and for OTA updates only
+	//
+	LEAF_WARN("Enabling WiFi leaf as secondary comms, for Debug and OTA only");
+	wifi->permitRun();
+      }
+      else if (wifi && app_use_wifi) {
+	// Wifi is the primary comms method
+	AbstractPubsubLeaf *wifi_pubsub =(AbstractPubsubLeaf *)find("wifimqtt", "pubsub");
+
+	if (!wifi || !wifi_pubsub) {
+	  LEAF_ALERT("Cannot find WiFi ip/pubsub leaf-pair");
+	}
+	else {
+
+	  LEAF_NOTICE("Selecting WiFi as preferred communications");
+	  // The wifi is the primary comms
+	  stacxSetComms(wifi, wifi_pubsub);
+	  ip_valid = true;
+	}
+      }
+      else if (wifi && !app_use_wifi) {
+	LEAF_NOTICE("Use of WiFi for WiFi comms is not enabled");
+      }
+
+      if (!ip_valid) {
+	// no comms preference, start the null comms modules if present
+	AbstractIpLeaf *nullip=(AbstractIpLeaf *)find("nullip","ip");
+	AbstractPubsubLeaf *nullps=(AbstractPubsubLeaf *)find("nullmqtt","pubsub");
+	if (nullip && nullps) {
+	  LEAF_NOTICE("Configuring null communications (local messages only)");
+	  stacxSetComms(nullip, nullps);
+	  ip_valid=true;
+	}
+      }
+
+      if (!ip_valid) {
+	LEAF_ALERT("No valid communications module selected (not even null)");
+      }
+    }
     
     LEAF_LEAVE;
   }
