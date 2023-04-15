6765487c956ba058159ab3d24729120b127acbd0
Christopher Biggs <christopher@accelerando.com.au>
2023-04-15 10:55:04 +1000

    Update examples

--- examples/example-lightswitch-button.ino
+++ examples/example-lightswitch-button.ino
@@ -13,13 +13,13 @@
 
 Leaf *leaves[] = {
 	new FSPreferencesLeaf("prefs"),
-	new IpEspLeaf("esp","prefs"),
-	new PubsubEspAsyncMQTTLeaf("espmqtt","prefs"),
+	new IpEspLeaf("wifi","prefs"),
+	new PubsubEspAsyncMQTTLeaf("wifimqtt","prefs,wifi"),
 	new ShellLeaf("shell"),
 
 	new LightLeaf("light",  "app", LEAF_PIN( 5 /* D1 OUT */)),
-	new ButtonLeaf("button", LEAF_PIN( 4 /* D2  IN */), HIGH),
-	new MotionLeaf("motion", LEAF_PIN( 0 /* D3  IN */)),
+	new ButtonLeaf("button", LEAF_PIN( 0 /* D3  IN */), HIGH),
+	new MotionLeaf("motion", LEAF_PIN( 2 /* D4  IN */)),
 
 	new LightswitchAppLeaf("app", "light,button,motion"),
 	NULL
