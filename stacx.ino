bff119a680f49cb4bffdcdca848f347a054431c4
Christopher Biggs <christopher@accelerando.com.au>
2023-04-15 10:56:15 +1000

    stacx.ino - Update skeleton example

--- stacx.ino
+++ stacx.ino
@@ -1,15 +1,6 @@
 #include "config.h"
 #include "stacx.h"
 
-
-//
-// Example stack: A smart light switch which operates autonomously, but also
-//                interacts with a building controller via MQTT.
-//
-// You should define your stack here (or you can incorporate stacx as a git 
-// submodule in your project by including stacx/stacx.h
-//
-
 #include "leaf_fs_preferences.h"
 #include "leaf_ip_esp.h"
 #include "leaf_pubsub_mqtt_esp.h"
@@ -24,12 +15,12 @@
 Leaf *leaves[] = {
 	new FSPreferencesLeaf("prefs"),
 	new IpEspLeaf("wifi","prefs"),
-	new PubsubEspAsyncMQTTLeaf("wifimqtt","prefs"),
+	new PubsubEspAsyncMQTTLeaf("wifimqtt","prefs,wifi"),
 	new ShellLeaf("shell"),
 
-	new LightLeaf("light",  "app", LEAF_PIN( 5 /* D1 OUT */), true),
-	new ButtonLeaf("button", LEAF_PIN( 4 /* D2  IN */), HIGH),
-	new MotionLeaf("motion", LEAF_PIN( 0 /* D3  IN */)),
+	new LightLeaf("light",  "app", LEAF_PIN( 5 /* D1 OUT */)),
+	new ButtonLeaf("button", LEAF_PIN( 0 /* D3  IN */), HIGH),
+	//new MotionLeaf("motion", LEAF_PIN( 2 /* D4  IN */)),
 
 	new LightswitchAppLeaf("app", "light,button,motion"),
 	NULL
