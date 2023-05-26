03f0b72e796fb7ae09ec768be5cd22c30b1f6508
Christopher Biggs <christopher@accelerando.com.au>
2023-05-26 13:18:44 +1000

    leaf_scd40.h - Option to use SparkFun library for SCD40

--- leaf_scd40.h
+++ leaf_scd40.h
@@ -7,20 +7,37 @@
 
 #include <Wire.h>
 #include "abstract_temp.h"
+
+#define USE_SCD40_SPARKFUN_LIBRARY 1
+
+#ifdef USE_SCD40_SENSIRION_LIBRARY
 #include <SensirionI2CScd4x.h>
+#elif defined(USE_SCD40_SPARKFUN_LIBRARY)
+#include <SparkFun_SCD4x_Arduino_Library.h>
+#else
+#error "No library choice defined"
+#endif
+
 #include "trait_wirenode.h"
 
 class Scd40Leaf : public AbstractTempLeaf, public WireNode
 {
 public:
+#ifdef USE_SCD40_SENSIRION_LIBRARY
   SensirionI2CScd4x scd40;
-
+#elif defined(USE_SCD40_SPARKFUN_LIBRARY)
+  SCD4x scd40;
+#endif
+  
   Scd40Leaf(String name, int address=0x62)
     : AbstractTempLeaf(name, 0)
     , WireNode(name, address)
     , Debuggable(name)
  {
     LEAF_ENTER(L_INFO);
+#ifdef USE_SCD40_SPARKFUN_LIBRARY
+    sample_interval_ms = 5000;
+#endif
     LEAF_LEAVE;
   }
 
@@ -28,9 +45,9 @@ public:
     LEAF_ENTER(L_INFO);
     AbstractTempLeaf::setup();
     
-    LEAF_INFO("scd40.begin");
+#ifdef USE_SCD40_SENSIRION_LIBRARY
+    LEAF_INFO("scd40.begin (Sensirion library)");
     scd40.begin(Wire);
-
     uint16_t serial0;
     uint16_t serial1;
     uint16_t serial2;
@@ -51,6 +68,15 @@ public:
       LEAF_ALERT("Error trying to execute startPeriodicMeasurement");
     }
 
+#elif defined(USE_SCD40_SPARKFUN_LIBRARY)
+    LEAF_INFO("scd40.begin (SparkFun library)");
+    if (scd40.begin() == false) {
+      LEAF_ALERT("SCD40 initialisation failed");
+      stop();
+      return;
+    }
+    LEAF_NOTICE("Scd40 at I2C 0x%02x");
+#endif
 
     LEAF_LEAVE;
   }
@@ -66,6 +92,7 @@ public:
     uint16_t error;
     bool changed = false;
 
+#ifdef USE_SCD40_SENSIRION_LIBRARY
     error = scd40.getDataReadyFlag(isDataReady);
     if (error) {
       LEAF_WARN("sc4x is not able to provide status");
@@ -85,6 +112,16 @@ public:
       LEAF_WARN("Invalid sample detected, skipping.");
       return false;
     }
+#elif defined(USE_SCD40_SPARKFUN_LIBRARY)
+    if (!scd40.readMeasurement()) {
+      LEAF_ALERT("scd40 readMeasurement failed");
+      return false;
+    }
+    new_co2 = scd40.getCO2();
+    new_temperature = scd40.getTemperature();
+    new_humidity= scd40.getHumidity();
+#endif
+    LEAF_DEBUG("SCD40 reading %dppm temp=%.1f hum=%.1f", (int)new_co2, new_temperature, new_humidity);
 
     if (isnan(*h) || (*h != new_humidity)) {
       *h = new_humidity;
@@ -94,7 +131,6 @@ public:
       *t = new_temperature;
       changed = true;
     }
-
     if (isnan(ppmCO2) || (ppmCO2 != new_co2)) {
       ppmCO2 = new_co2;
       changed = true;
@@ -102,7 +138,6 @@ public:
 
     RETURN(changed);
   }
-
 };
 
 // local Variables:
