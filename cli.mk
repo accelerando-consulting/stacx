0fc422dd28540cef648bcdc3880b671ad1624636
Christopher Biggs <christopher@accelerando.com.au>
2023-05-30 10:15:08 +1000

    cli.mk - Merge the makefile variables DEVICE and DEVICE_ID to a single DEVICE_ID

--- cli.mk
+++ cli.mk
@@ -115,8 +115,8 @@ clean:
 	rm -f $(OBJ)
 
 ota: $(OBJ)
-	@if [ -z "$$IP" ] ; then \
-		IP=`avahi-browse -ptr  "_arduino._tcp" | egrep ^= | cut -d\; -f4,8,9 | grep ^$$DEVICE | cut -d\; -f2` -p `avahi-browse -ptr  "_arduino._tcp" | egrep ^= | cut -d\; -f4,8,9 | grep ^$$DEVICE | cut -d\; -f3` ;\
+	@if [ -z "$${IP}" ] ; then \
+		IP=`avahi-browse -ptr  "_arduino._tcp" | egrep ^= | cut -d\; -f4,8,9 | grep ^$${DEVICE_ID} | cut -d\; -f2` -p `avahi-browse -ptr  "_arduino._tcp" | egrep ^= | cut -d\; -f4,8,9 | grep ^$${DEVICE_ID} | cut -d\; -f3` ;\
 	fi ;\
 	python $(OTAPROG) -i $(IP) "--auth=$(OTAPASS)" -f $(OBJ)
 
