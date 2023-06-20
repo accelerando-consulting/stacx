101009dd31992daa32ce1ae8c105bd779562cdaa
Christopher Biggs <christopher@accelerando.com.au>
2023-06-20 11:10:51 +1000

    leaf_dht12.h - probe for device and abort if not found

--- leaf_dht12.h
+++ leaf_dht12.h
@@ -9,13 +9,14 @@
 #include <DHT12.h>
 #include "abstract_temp.h"
 
-class Dht12Leaf : public AbstractTempLeaf
+class Dht12Leaf : public AbstractTempLeaf, public WireNode
 {
 public:
-  DHT12 dht12;
+  DHT12 *dht12=NULL;
  
-  Dht12Leaf(String name, pinmask_t pins=0)
-    : AbstractTempLeaf(name, pins)
+  Dht12Leaf(String name, int address = 0x5c)
+    : AbstractTempLeaf(name, NO_PINS)
+    , WireNode(name, address)
     , Debuggable(name)
   {
     LEAF_ENTER(L_INFO);
@@ -25,14 +26,22 @@ public:
   void setup(void) {
     LEAF_ENTER(L_INFO);
     Leaf::setup();
-    dht12.begin();
+
+    if (!probe(address)) {
+      LEAF_ALERT("DHT12 not found at 0x%02X", (int)address);
+      stop();
+      return;
+    }
+
+    dht12 = new DHT12(address);
+    dht12->begin();
     LEAF_LEAVE;
   }
 
   bool poll(float *h, float *t, const char **status) 
   {
-    *h = dht12.readHumidity();
-    *t = dht12.readTemperature();
+    *h = dht12->readHumidity();
+    *t = dht12->readTemperature();
     if (isnan(*h) || isnan(*t)) {
       LEAF_ALERT("Failed to read from DHT12 sensor");
       *status = "readfail";
