d2529381b47582b53868cfe076c02b4505d98d04
Christopher Biggs <christopher@accelerando.com.au>
2024-02-03 10:22:16 +1000

    leaf_lvgl_cyb28.h - Get CYB28 touch screen working

--- leaf_lvgl_cyb28.h
+++ leaf_lvgl_cyb28.h
@@ -2,7 +2,7 @@
 #include "leaf_lvgl.h"
 #include <TFT_Touch.h>
 
-// 
+//
 // The Cheap Yellow Board identifying itself as "2.8inch_ESP32-2432S028R"
 // uses a separate SPI bus for its touch controller, which violates TFT_eSPI's
 // assumption that the display and touch panel share the same SPI, differing only
@@ -20,29 +20,25 @@ void _cyb28_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data
       data->state = LV_INDEV_STATE_REL;
       return;
     }
-    
+
     bool touched = _cyb28_touch->Pressed();
 
     if( !touched )
     {
-        data->state = LV_INDEV_STATE_REL;
+	data->state = LV_INDEV_STATE_REL;
     }
     else
-    {   
-        touchX = _cyb28_touch->X();
-        touchY = _cyb28_touch->Y();
-        
-        data->state = LV_INDEV_STATE_PR;
+    {
+	touchX = _cyb28_touch->X();
+	touchY = _cyb28_touch->Y();
 
-        /*Set the coordinates*/
-        data->point.x = touchX;
-        data->point.y = touchY;
+	data->state = LV_INDEV_STATE_PR;
 
-        Serial.print( "Data x " );
-        Serial.println( touchX );
+	/*Set the coordinates*/
+	data->point.x = touchX;
+	data->point.y = touchY;
 
-        Serial.print( "Data y " );
-        Serial.println( touchY );
+	DEBUG("touch event x=%d y=%d", (int)touchX, (int)touchY);
     }
 }
 
@@ -51,9 +47,9 @@ class CYB28Leaf : public LVGLLeaf
 protected:
   int touch_dout = 39;
   int touch_din = 32;
-  int touch_cs = 32;
+  int touch_cs = 33;
   int touch_clk = 25;
-	
+
   TFT_Touch *touch = NULL;
   lv_indev_drv_t cyb28_indev_drv;
 
@@ -72,25 +68,28 @@ public:
 
     // TODO read settings
 
-    LEAF_NOTICE("tft touch");
-    // reject the superclass' input driver and substitute our own
+    LEAF_NOTICE("  tft touch setup");
     _cyb28_touch = touch = new TFT_Touch(touch_cs, touch_clk, touch_din, touch_dout);
+    touch->setCal(526, 3443, 750, 3377, 320, 240, 1);
+    touch->setRotation(rotation);
+
+    LEAF_NOTICE("  LVGL input driver setup");
+    // reject the superclass' input driver and substitute our own
     lv_indev_drv_init(&cyb28_indev_drv);
     cyb28_indev_drv.type = LV_INDEV_TYPE_POINTER;
     cyb28_indev_drv.read_cb = _cyb28_touchpad_read;
     lv_indev_drv_update( get_indev(), &cyb28_indev_drv );
 
-    //TFT_Touch(touch_cs, touch_clk, touch_din, touch_dout);
     LEAF_LEAVE;
   }
 
-  virtual void start(void) 
+  virtual void start(void)
   {
     LEAF_ENTER(L_NOTICE);
     LVGLLeaf::start();
     LEAF_LEAVE;
   }
-  
+
 
 };
 
