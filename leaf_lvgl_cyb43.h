cbd872c92495d725c183f933925824611dbbdba3
Christopher Biggs <christopher@accelerando.com.au>
2024-02-19 14:33:24 +1000

    leaf_lvgl_cyb43.h - Support for 800 and 480 px CYB43 variants

--- leaf_lvgl_cyb43.h
+++ leaf_lvgl_cyb43.h
@@ -176,6 +176,27 @@ public:
 
     LEAF_NOTICE("  GFX allocations");
 
+
+#ifdef SCREEN_CYB4827S043
+    LEAF_WARN("using 480x272 screen");
+  // Crummy low res version  ILI6485 LCD 480x272
+  rgbpanel = new Arduino_ESP32RGBPanel(
+    40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
+    45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
+    5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
+    8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */,
+    0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 43 /* hsync_back_porch */,
+    0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
+    1 /* pclk_active_neg */, 9000000 /* prefer_speed */
+    );
+
+  ::gfx = gfx = new Arduino_RGB_Display(
+    480 /* width */, 272 /* height */, rgbpanel, rotation, true /* auto_flush */);
+
+#else
+    LEAF_WARN("using 800x480 screen");
+
+  // Shiny hi-res version
     rgbpanel = new Arduino_ESP32RGBPanel(
       40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
       45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
@@ -187,7 +208,8 @@ public:
 
     ::gfx = gfx = new Arduino_RGB_Display(
       800 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */);
-
+#endif
+    
     LEAF_NOTICE("  GFX panel setup");
     if (!gfx->begin())
     {
@@ -223,7 +245,11 @@ public:
     gfx->fillScreen(WHITE);
 
     gfx->setCursor(20, 20);
+#ifdef SCREEN_CYB4827043
+    gfx->setTextSize(2);
+#else
     gfx->setTextSize(3);
+#endif
     gfx->setTextColor(BLACK);
     gfx->println(device_id);
     delay(750);
