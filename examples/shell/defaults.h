54049182dcbbe9c6364a2304527e303f3cccc4ae
Christopher Biggs <christopher@accelerando.com.au>
2024-05-24 15:24:36 +1000

    defaults.h - Only try to use pin 9 as RGB on C3 boards

--- examples/shell/defaults.h
+++ examples/shell/defaults.h
@@ -7,7 +7,10 @@
 #define USE_CPU_TEMP 1
 #define IP_ENABLE_OTA true
 
+#ifdef ARDUINO_ESP32C3_DEV
 #define HELLO_PIXEL 9
+#endif
+
 #define USE_TELNETD 1
 #define USE_TELNETD_DEFAULT true
 #define IP_WIFI_OWN_LOOP true
