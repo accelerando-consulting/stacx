82d0dd23d49b455b5ac2bc68bb171936a848fcbe
Christopher Biggs <christopher@accelerando.com.au>
2024-10-02 10:40:14 +1000

    leaf_beeper.h - accelerando daisy i2c speaker module

---
+++ leaf_beeper.h
@@ -0,0 +1,299 @@
+//@***************************** class BeeperLeaf ******************************
+//
+// An I2C attached beep generator
+//
+
+#include "pitches.h"
+#include "trait_wirenode.h"
+
+class BeeperLeaf : public Leaf, public WireNode
+{
+public:
+  int freq;
+  int duration;
+  bool do_test = false;
+  int tempo=60;
+  String tune="";
+
+  const int REG_VENDOR_ID   = 0x00;
+  const int REG_VENDOR_ID2  = 0x01;
+  const int REG_VENDOR_ID3  = 0x02;
+  const int REG_DEVICE_TYPE = 0x03;
+  const int REG_DEVICE_ADDR = 0x04;
+
+  const int REG_COMMAND     = 0x10;
+  const int REG_STATE       = 0x11;
+
+  const int REG_FREQ        = 0x20;
+  const int REG_DURATION    = 0x21;
+  const int REG_TEMPO       = 0x22;
+  const int REG_VOLUME      = 0x23;
+
+  const int REG_TUNE_COUNT  = 0x30;
+  const int REG_TUNE__MAX   = 0x31;
+  const int REG_TUNE_IDX    = 0x32;
+  const int REG_TUNE_LEN    = 0x33;
+  const int REG_TUNE        = 0x34;
+  
+  const int CMD_STOP = 0x01;
+  const int CMD_BEEP = 0x02;
+  const int CMD_TONE = 0x02;
+  const int CMD_TUNE = 0x03;
+
+  const int STATE_BIT_PLAYING = 0x01;
+  
+  BeeperLeaf(String name, int address=0x03, Twiwire *bus=&Wire)
+    : Leaf("beeper", name, NO_PINS)
+    , WireNode(name, address, bus)
+    , Debuggable(name)
+  {
+    this->freq = freq;
+    this->duration = duration;
+    this->do_test = do_test;
+  }
+
+  virtual void setup(void) {
+    Leaf::setup();
+
+    if (probe(address)) {
+      LEAF_WARN("Failed to detect beeper at 0x%02x", address);
+      stop();
+      return;
+    }
+
+    registerCommand(HERE, "tone", "Play a tone given freq,duration");
+    registerCommand(HERE, "stop", "Stop playing tone(s)");
+
+    registerCommand(HERE, "beep", "Play a beep at the default pitch and duration");
+    registerCommand(HERE, "note", "Play a note denoted by pitch and octave, eg C3");
+    registerCommand(HERE, "tune", "Play a series of notes eg G4 A4 F4:2 R F3 C4:2");
+    registerIntValue("tempo", &tempo, "Note tempo in beats per minute");
+    registerIntValue("freq", &freq, "Note frequency in Hz");
+    registerIntValue("duration", &duration, "Default tone frequency in milliseconds");
+  }
+
+  void stopTone()
+  {
+    write_register(REG_COMMAND, CMD_STOP);
+  }
+
+  void beep() 
+  {
+    write_register(REG_COMMAND, CMD_BEEP);
+  }
+
+  void writeCommandDoubleword(uint8_t reg, uint8_t cmd, uint16_t arg1, uint16_t arg2) 
+  {
+    wire->beginTransmission(address);
+    wire->write(reg);
+    wire->write(cmd);
+    wire->write((arg1>>8)&0xFF);
+    wire->write(arg1&0XFF);
+    wire->write((arg2>>8)&0xFF);
+    wire->write(arg2&0XFF);
+    wire->endTransmission();
+  }
+
+  
+  void playTone(int pitch, int len)
+  {
+    if (len <= 1) len = duration;
+    if (pitch == 0) pitch=freq;
+
+    writeCommandDoubleword(REG_COMMAND, CMD_TONE, (uint16_t)pitch, (uint16_t)len);
+  }
+
+  void playNote(String note, float beats)
+  {
+    LEAF_ENTER(L_INFO);
+    if (note.length()) {
+      LEAF_NOTICE("playNote %s, %f", note.c_str(), beats);
+    }
+    else {
+      LEAF_ALERT("Empty note definition");
+      LEAF_VOID_RETURN;
+    }
+    //UPTOHERE ran out of brain
+
+    wire->beginTransmission(address);
+    wire->write(reg);
+    wire->write(cmd);
+    wire->write((arg1>>8)&0xFF);
+    wire->write(arg1&0XFF);
+    wire->write((arg2>>8)&0xFF);
+    wire->write(arg2&0XFF);
+    wire->endTransmission();
+
+
+    setBusy();
+    int pin = tonePin;
+    int octave=1;
+    int key;
+    int pos=0;
+
+    switch (toupper(note[pos++])) {
+    case 'C':
+      key=1;
+      break;
+    case 'D':
+      key=3;
+      break;
+    case 'E':
+      key=5;
+      break;
+    case 'F':
+      key=6;
+      break;
+    case 'G':
+      key=8;
+      break;
+    case 'A':
+      key=10;
+      break;
+    case 'B':
+      key=12;
+      break;
+    case 'R':
+      key=-1;
+      break;
+    default:
+      LEAF_ALERT("Invalid note [%s]", note.c_str());
+      LEAF_VOID_RETURN;
+    }
+    switch (note[pos]) {
+    case 'S':
+      key++;
+      pos++;
+      break;
+    case 'F':
+      key--;
+      pos++;
+    }
+
+    if (key >= 0) {
+      // i.e not a rest
+      if ((note[pos]<'0') || (note[pos]>'8')) {
+	LEAF_ALERT("Invalid note [%s]", note.c_str());
+	LEAF_VOID_RETURN;
+      }
+      octave = note[pos]-'0';
+
+      while (octave < 1) {
+	key-=12;
+	octave++;
+      }
+      while (octave > 1) {
+	key+=12;
+	octave--;
+      }
+      if ((key < 0) || (key>88)) {
+	LEAF_ALERT("Invalid note [%s]", note.c_str());
+      }
+    }
+
+
+    int freq = (key==-1)?-1:sNotePitches[key];
+    int ms = beats * 60000/tempo;
+    //LEAF_DEBUG("Note %s is key %d (%dHz).  %.3f beats is %dms", note.c_str(), key, freq, beats, ms);
+    playTone(freq, ms);
+    LEAF_VOID_RETURN;
+  }
+
+  void playTune(String tune)
+  {
+    // this could be called from interrupt context, so be light on the logging, OK?
+    LEAF_ENTER_STR(L_DEBUG, tune);
+    String note;
+    int pos;
+    float beats;
+
+    if ((pos = tune.indexOf(" ")) > 0) {
+      note = tune.substring(0,pos);
+      tune.remove(0,pos+1);
+    }
+    else {
+      note = tune;
+      tune="";
+    }
+
+    if ((pos = note.indexOf(",")) > 0) {
+      beats = note.substring(pos+1).toFloat();
+      note.remove(pos);
+    }
+    else {
+      beats = 1;
+    }
+
+    setBusy();
+    this->tune=String(tune);
+    //LEAF_DEBUG("Playing note %s,%f, remainder of tune is %s", note.c_str(), beats, this->tune.c_str());
+    playNote(note, beats);
+    LEAF_LEAVE;
+  }
+
+
+  virtual void start(void)
+  {
+    Leaf::start();
+    if (do_test) {
+      LEAF_NOTICE("Test speaker");
+      playTone(0,250);
+    }
+  }
+
+  virtual bool commandHandler(String type, String name, String topic, String payload) {
+    LEAF_HANDLER(L_INFO);
+
+    WHEN("note",{
+	if (isBusy()) {
+	  LEAF_NOTICE("Speaker busy");
+	}
+	else {
+	  int space = payload.indexOf(" ");
+	  float beats = 1;
+	  if (space>0) {
+	    beats = payload.substring(space+1).toFloat();
+	    payload.remove(space);
+	  }
+	  playNote(payload, beats);
+	}
+      })
+    ELSEWHEN("tune",{
+	if ((payload.length() == 0) || (payload=="1")) {
+	  payload = "G4 A4 F4:2 R F3 C4:2";
+	}
+	playTune(payload);
+      })
+    ELSEWHEN("stop",{
+	stopTone();
+      })
+    ELSEWHEN("tone",{
+	if (isBusy()) {
+	  LEAF_NOTICE("Speaker busy");
+	}
+	else {
+	  int freq = 0;
+	  int duration = 0;
+	  int comma = payload.indexOf(",");
+	  if (comma) {
+	    freq = payload.toInt();
+	    duration = payload.substring(comma+1).toInt();
+	  }
+	  setBusy();
+	  playTone(freq, duration);
+	  //LEAF_INFO("Tone playing in background");
+	}
+    })
+    else {
+      handled = Leaf::commandHandler(type, name, topic, payload);
+    }
+
+    LEAF_HANDLER_END;
+  }
+
+};
+
+// local Variables:
+// mode: C++
+// c-basic-offset: 2
+// End:
