533d002cfba84e5dda76e237c653caf04feeb8aa
Kit Biggs <kit@accelerando.com.au>
2025-07-11 11:41:04 +1000

    trait_polynomial.h - Add vsetup method. Fix computation, adjust logging.

--- trait_polynomial.h
+++ trait_polynomial.h
@@ -5,30 +5,60 @@
 #include <malloc.h>
 #include <math.h>
 
-class Polynomial
+class Polynomial: public Debuggable
 {
 protected:
-  int polynomial_degree=0;
+  int polynomial_degree=-1;
   double *polynomial_coefficients=NULL;
-  
-  void polynomial_setup(int degree, ...) 
+
+  void polynomial_allocate(int degree)
+  {
+    if (polynomial_coefficients && (polynomial_degree==degree)) {
+      // storage is already good
+      return;
+    }
+    if (polynomial_coefficients) {
+      free(polynomial_coefficients);
+    }
+    polynomial_degree = degree;
+    polynomial_coefficients = (double*)malloc((degree+1) * sizeof(double));
+  }
+
+public:
+  Polynomial(String name, int l=L_USE_DEFAULT)
+    : Debuggable(name, l)
+  {
+  }
+
+  void polynomial_setup(int degree, ...)
   {
     va_list ap;
     va_start(ap, degree);
-    this->polynomial_degree = degree;
-    this->polynomial_coefficients = (double*)malloc((degree+1) * sizeof(double));
+    polynomial_allocate(degree);
     for (int n=degree; n>=0; n--) {
       this->polynomial_coefficients[n] = va_arg(ap, double);
+      LEAF_NOTICE(" coefficient %d: %lf", n, this->polynomial_coefficients[n]);
     }
     va_end(ap);
   }
-  
-  double polynomial_compute(double x) 
+
+  void polynomial_vsetup(int degree, double *coefficients)
+  {
+    polynomial_allocate(degree);
+    for (int n=degree; n>=0; n--) {
+      this->polynomial_coefficients[n] = coefficients[n];
+      LEAF_NOTICE(" coefficient %d: %lf", n, this->polynomial_coefficients[n]);
+    }
+  }
+
+  double polynomial_compute(double x)
   {
     double y = 0;
+    LEAF_INFO(" x=%lf", x);
     for (int n=this->polynomial_degree; n>=0;n--) {
       if (this->polynomial_coefficients[n]==0) continue;
       y += polynomial_coefficients[n]*pow(x,n);
+      LEAF_INFO("  y += %lf * x^^%d => %lf", polynomial_coefficients[n], n, y);
     }
     return y;
   }
